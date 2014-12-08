e948211a4d8ae70ae4e3ccf634be4d30ede4bf22
Brendan Molloy <brendan@bbqsrc.net>
2014-12-09 01:30:50 +1100

    just leave it as keyNames

--- gen.py
+++ gen.py
@@ -643,7 +643,7 @@ class AppleiOSGenerator(Generator):
         import UIKit
 
         class %s: GiellaKeyboard {
-            var l10nKeys = ["keyboard": "%s", "return": "%s", "space": "%s"]
+            var keyNames = ["keyboard": "%s", "return": "%s", "space": "%s"]
 
             required init(coder: NSCoder) {
                 fatalError("init(coder:) has not been implemented")
@@ -694,7 +694,7 @@ class AppleiOSGenerator(Generator):
         buf.write(indent(dedent("""\
             if isPad {
                 var returnKey = Key(.Return)
-                returnKey.uppercaseKeyCap = l10nKeys["return"]
+                returnKey.uppercaseKeyCap = keyNames["return"]
                 returnKey.uppercaseOutput = "\\n"
                 returnKey.lowercaseOutput = "\\n"
 
@@ -717,7 +717,7 @@ class AppleiOSGenerator(Generator):
                 kbd.addKey(Key(.Backspace), row: 2, page: 0)
             }
 
-            super.init(keyboard: kbd, l10nKeys: l10nKeys)
+            super.init(keyboard: kbd, keyNames: keyNames)
         }
 
         class func getLongPresses() -> [String: [String]] {
