9cc9a77142cb3a5ebf0bc434298b0ac5985d7bf0
Pascal Hertleif <pascal@technocreatives.com>
2019-07-04 10:51:42 +0200

    Parse key values as strings
    Having some fun with regexes along the way

--- src/bundle/keys.rs
+++ src/bundle/keys.rs
@@ -1,13 +1,14 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use serde::{
     de::{self, Deserializer},
     ser::Serializer,
     Deserialize, Serialize,
 };
-use snafu::{ResultExt, Snafu};
-use std::str::FromStr;
+use snafu::Snafu;
 
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct KeyValue(pub(crate) Option<char>);
+pub struct KeyValue(pub(crate) Option<String>);
 
 impl<'de> Deserialize<'de> for KeyValue {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
@@ -29,52 +30,47 @@ impl Serialize for KeyValue {
     }
 }
 
-pub fn deserialize(input: &str) -> Result<Option<char>, Error> {
-    let input_char = char_from_str(&input)?;
-    if input_char == '\u{0}' {
+pub fn deserialize(input: &str) -> Result<Option<String>, Error> {
+    if input == r"\u{0}" {
         Ok(None)
     } else {
-        Ok(Some(input_char))
+        Ok(Some(decode_unicode_escapes(input)?))
     }
 }
 
-pub fn serialize(input: &Option<char>) -> String {
+pub fn serialize(input: &Option<String>) -> String {
     if let Some(input) = input {
-        let char_category = unic_ucd_category::GeneralCategory::of(*input);
+        input
+            .chars()
+            .map(|c| {
+                let char_category = unic_ucd_category::GeneralCategory::of(c);
 
-        if char_category.is_other() || char_category.is_separator() || char_category.is_mark() {
-            input.escape_unicode().to_string()
-        } else {
-            input.to_string()
-        }
+                if char_category.is_other()
+                    || char_category.is_separator()
+                    || char_category.is_mark()
+                {
+                    input.escape_unicode().to_string()
+                } else {
+                    input.to_string()
+                }
+            })
+            .collect()
     } else {
-        String::from("\u{0}")
+        String::from(r"\u{0}")
     }
 }
 
-fn char_from_str(input: &str) -> Result<char, Error> {
-    let first_try = char::from_str(&input).context(CharParseError { input });
-    if let Ok(c) = first_try {
-        return Ok(c);
+fn decode_unicode_escapes(input: &str) -> Result<String, Error> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"\\u\{([0-9A-Fa-f]{2,6})\}").expect("valid regex");
     }
 
-    match dbg!((input.find("\\u{"), input.rfind("}"))) {
-        (Some(0), Some(end)) => {
-            let inner = &input[3..end];
-            let number =
-                dbg!(u32::from_str_radix(inner, 16)).map_err(|e| Error::CharFromStrError {
-                    input: input.to_string(),
-                    description: e.to_string(),
-                })?;
-            Ok(
-                dbg!(std::char::from_u32(number)).ok_or_else(|| Error::CharFromStrError {
-                    input: input.to_string(),
-                    description: String::from("not enough char"),
-                })?,
-            )
-        }
-        _ => Ok(first_try?),
-    }
+    let new = RE.replace_all(input, |hex: &regex::Captures| {
+        let number = u32::from_str_radix(hex.get(1).unwrap().as_str(), 16).unwrap();
+        std::char::from_u32(number).unwrap().to_string()
+    });
+
+    Ok(new.to_string())
 }
 
 #[derive(Debug, Snafu)]
