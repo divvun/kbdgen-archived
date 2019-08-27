089941a1fd1a0446c2c9322fffb6d64c2c1ea6ea
Pascal Hertleif <pascal@technocreatives.com>
2019-08-27 15:33:55 +0200

    Move padding formatter to separate module

--- src/cldr/ser.rs
+++ src/cldr/ser.rs
@@ -1,5 +1,5 @@
 use super::*;
-use pad::PadAdapter;
+use crate::pad::PadAdapter;
 use std::io::{Result, Write};
 use xml::escape::escape_str_attribute as escape;
 
@@ -90,55 +90,5 @@ impl ToXml for Map {
         writeln!(w, r#"/>"#)?;
 
         Ok(())
-    }
-}
-
-// adopted from libcore's `fmt/builders.rs`
-pub(crate) mod pad {
-    use std::io::{Result, Write};
-
-    /// Indents all lines written to this by four spaces
-    pub(crate) struct PadAdapter<'a> {
-        buf: &'a mut (dyn Write + 'a),
-        on_newline: bool,
-    }
-
-    impl<'a> PadAdapter<'a> {
-        pub(crate) fn wrap<'fmt: 'a>(buf: &'fmt mut (dyn Write + 'a)) -> Self {
-            PadAdapter {
-                buf,
-                on_newline: true,
-            }
-        }
-    }
-
-    impl Write for PadAdapter<'_> {
-        fn write(&mut self, mut s: &[u8]) -> Result<usize> {
-            let len = s.len();
-            while !s.is_empty() {
-                if self.on_newline {
-                    self.buf.write_all(b"    ")?;
-                }
-
-                let split = match s.iter().enumerate().find(|(_, byte)| **byte == b'\n') {
-                    Some((pos, _)) => {
-                        self.on_newline = true;
-                        pos + 1
-                    }
-                    None => {
-                        self.on_newline = false;
-                        s.len()
-                    }
-                };
-                self.buf.write_all(&s[..split])?;
-                s = &s[split..];
-            }
-
-            Ok(len)
-        }
-
-        fn flush(&mut self) -> Result<()> {
-            Ok(())
-        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ pub mod bundle;
 pub mod cldr;
 
 pub use bundle::*;
+
+pub(crate) mod pad;
