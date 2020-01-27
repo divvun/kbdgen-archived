4196b77a25ce1f9aa46eccc57ece7f19e77955f0
Pascal Hertleif <pascal@technocreatives.com>
2020-01-27 11:58:35 +0100

    Reduce duplication in CLI code
    This is what I do

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,14 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 enum IOSCommands {
     Init,
-    Ids
+    Ids,
 }
 
 #[derive(Debug, StructOpt)]
 enum BuildCommands {
     Svg {
-        #[structopt(short, long = "output", default_value = ".", parse(from_os_str))]
-        output_path: PathBuf,
-
-        #[structopt(parse(from_os_str))]
-        project_path: PathBuf,
+        #[structopt(flatten)]
+        in_out: InOutPaths,
     },
     Android {
         #[structopt(long = "kbd-repo", default_value = "divvun/giellakbd-android")]
@@ -30,20 +27,14 @@ enum BuildCommands {
         #[structopt(long = "divvunspell-branch", default_value = "develop")]
         divvunspell_branch: String,
 
-        #[structopt(short, long = "output", parse(from_os_str))]
-        output_path: PathBuf,
+        #[structopt(flatten)]
+        in_out: InOutPaths,
 
         #[structopt(short = "D", long = "dry-run")]
         dry_run: bool,
 
-        #[structopt(short = "R", long = "release")]
-        release: bool,
-
-        #[structopt(long = "ci")]
-        ci: bool,
-
-        #[structopt(parse(from_os_str))]
-        project_path: PathBuf,
+        #[structopt(flatten)]
+        build_mode: BuildMode,
     },
     IOS {
         #[structopt(subcommand)]
@@ -55,112 +46,90 @@ enum BuildCommands {
         #[structopt(long = "kbd-branch", default_value = "develop")]
         kbd_branch: String,
 
-        #[structopt(short, long = "output", default_value = ".", parse(from_os_str))]
-        output_path: PathBuf,
+        #[structopt(flatten)]
+        in_out: InOutPaths,
 
         #[structopt(short = "D", long = "dry-run")]
         dry_run: bool,
 
-        #[structopt(short = "R", long = "release")]
-        release: bool,
-
-        #[structopt(long = "ci")]
-        ci: bool,
-
-        #[structopt(parse(from_os_str))]
-        project_path: PathBuf,
+        #[structopt(flatten)]
+        build_mode: BuildMode,
     },
     Win {
-        #[structopt(short, long = "output", default_value = ".", parse(from_os_str))]
-        output_path: PathBuf,
+        #[structopt(flatten)]
+        in_out: InOutPaths,
 
         #[structopt(short = "D", long = "dry-run")]
         dry_run: bool,
 
-        #[structopt(short = "R", long = "release")]
-        release: bool,
-
-        #[structopt(long = "ci")]
-        ci: bool,
-
-        #[structopt(parse(from_os_str))]
-        project_path: PathBuf,
+        #[structopt(flatten)]
+        build_mode: BuildMode,
     },
     Mac {
-        #[structopt(short, long = "output", default_value = ".", parse(from_os_str))]
-        output_path: PathBuf,
+        #[structopt(flatten)]
+        in_out: InOutPaths,
 
         #[structopt(short = "D", long = "dry-run")]
         dry_run: bool,
 
-        #[structopt(short = "R", long = "release")]
-        release: bool,
-
-        #[structopt(long = "ci")]
-        ci: bool,
-
-        #[structopt(parse(from_os_str))]
-        project_path: PathBuf,
+        #[structopt(flatten)]
+        build_mode: BuildMode,
     },
     X11 {
-        #[structopt(short, long = "output", default_value = ".", parse(from_os_str))]
-        output_path: PathBuf,
+        #[structopt(flatten)]
+        in_out: InOutPaths,
 
-        #[structopt(short = "R", long = "release")]
-        release: bool,
-
-        #[structopt(long = "ci")]
-        ci: bool,
-
-        #[structopt(parse(from_os_str))]
-        project_path: PathBuf,
+        #[structopt(flatten)]
+        build_mode: BuildMode,
     },
     M17n {
-        #[structopt(short, long = "output", default_value = ".", parse(from_os_str))]
-        output_path: PathBuf,
+        #[structopt(flatten)]
+        in_out: InOutPaths,
 
-        #[structopt(short = "R", long = "release")]
-        release: bool,
-
-        #[structopt(long = "ci")]
-        ci: bool,
-
-        #[structopt(parse(from_os_str))]
-        project_path: PathBuf,
+        #[structopt(flatten)]
+        build_mode: BuildMode,
     },
     Chrome {
-        #[structopt(short, long = "output", default_value = ".", parse(from_os_str))]
-        output_path: PathBuf,
+        #[structopt(flatten)]
+        in_out: InOutPaths,
 
-        #[structopt(short = "R", long = "release")]
-        release: bool,
-
-        #[structopt(long = "ci")]
-        ci: bool,
-
-        #[structopt(parse(from_os_str))]
-        project_path: PathBuf,
+        #[structopt(flatten)]
+        build_mode: BuildMode,
     },
     Qr {
-        #[structopt(short, long = "output", default_value = ".", parse(from_os_str))]
-        output_path: PathBuf,
+        #[structopt(flatten)]
+        in_out: InOutPaths,
 
         #[structopt(short, long = "layout")]
         layout: String,
-
-        #[structopt(parse(from_os_str))]
-        project_path: PathBuf,
     },
     ErrorModel {
-        #[structopt(short, long = "output", default_value = ".", parse(from_os_str))]
-        output_path: PathBuf,
+        #[structopt(flatten)]
+        in_out: InOutPaths,
 
         #[structopt(short, long = "layout")]
         layout: String,
-
-        #[structopt(parse(from_os_str))]
-        project_path: PathBuf,
     },
+}
+
+#[derive(Debug, StructOpt)]
+struct InOutPaths {
+    #[structopt(short, long = "output", default_value = ".", parse(from_os_str))]
+    output_path: PathBuf,
+
+    #[structopt(parse(from_os_str))]
+    project_path: PathBuf,
+}
+
+#[derive(Debug, StructOpt)]
+struct BuildMode {
+    /// Compile in 'release' mode (where necessary).
+    #[structopt(short = "R", long = "release")]
+    release: bool,
+
+    /// Continuous integration build
+    #[structopt(long = "ci")]
+    ci: bool,
 }
 
 #[derive(Debug, StructOpt)]
@@ -176,8 +145,8 @@ enum NewCommands {
         #[structopt(short, long = "output", default_value = ".", parse(from_os_str))]
         output_path: PathBuf,
 
-        bundle_name: String
-    }
+        bundle_name: String,
+    },
 }
 
 #[derive(Debug, StructOpt)]
@@ -185,37 +154,45 @@ enum Commands {
     Build {
         #[structopt(long = "github-username")]
         github_username: Option<String>,
-    
+
         #[structopt(long = "github-token")]
         github_token: Option<String>,
-    
+
         #[structopt(subcommand)]
         command: BuildCommands,
     },
     New {
         #[structopt(subcommand)]
         command: NewCommands,
-    }
+    },
 }
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "kbdgen", about = "An example of StructOpt usage.")]
+#[structopt(name = "kbdgen")]
 struct Opts {
     #[structopt(long = "logging", default_value = "info")]
     logging: String,
 
     #[structopt(subcommand)]
-    command: Commands
+    command: Commands,
 }
 
 impl BuildCommands {
-    fn to_py_args<'a>(&'a self, github_username: Option<&'a str>, github_token: Option<&'a str>, logging: &'a str) -> Result<Vec<&'a str>, Box<dyn std::error::Error>> {
+    fn to_py_args<'a>(
+        &'a self,
+        github_username: Option<&'a str>,
+        github_token: Option<&'a str>,
+        logging: &'a str,
+    ) -> Result<Vec<&'a str>, Box<dyn std::error::Error>> {
         use BuildCommands::*;
 
         let mut args = match self {
             Svg {
-                output_path,
-                project_path,
+                in_out:
+                    InOutPaths {
+                        output_path,
+                        project_path,
+                    },
             } => vec![
                 "-t",
                 "svg",
@@ -228,11 +205,13 @@ impl BuildCommands {
                 kbd_branch,
                 divvunspell_repo,
                 divvunspell_branch,
-                output_path,
+                in_out:
+                    InOutPaths {
+                        output_path,
+                        project_path,
+                    },
                 dry_run,
-                release,
-                ci,
-                project_path,
+                build_mode: BuildMode { release, ci },
             } => {
                 let mut args = vec![
                     "-t",
@@ -269,11 +248,13 @@ impl BuildCommands {
                 command,
                 kbd_repo,
                 kbd_branch,
-                output_path,
+                in_out:
+                    InOutPaths {
+                        output_path,
+                        project_path,
+                    },
                 dry_run,
-                release,
-                ci,
-                project_path,
+                build_mode: BuildMode { release, ci },
             } => {
                 let mut args = vec![
                     "-t",
@@ -306,7 +287,7 @@ impl BuildCommands {
                     Some(IOSCommands::Ids) => {
                         args.push("--command");
                         args.push("ids");
-                    },
+                    }
                     _ => {}
                 }
 
@@ -314,10 +295,12 @@ impl BuildCommands {
                 args
             }
             Chrome {
-                output_path,
-                release,
-                ci,
-                project_path,
+                in_out:
+                    InOutPaths {
+                        output_path,
+                        project_path,
+                    },
+                build_mode: BuildMode { release, ci },
             } => {
                 let mut args = vec!["-t", "chrome", "-o", &*output_path.to_str().unwrap()];
 
@@ -333,11 +316,13 @@ impl BuildCommands {
                 args
             }
             Win {
-                output_path,
+                in_out:
+                    InOutPaths {
+                        output_path,
+                        project_path,
+                    },
                 dry_run,
-                release,
-                ci,
-                project_path,
+                build_mode: BuildMode { release, ci },
             } => {
                 let mut args = vec!["-t", "win", "-o", &*output_path.to_str().unwrap()];
 
@@ -357,11 +342,13 @@ impl BuildCommands {
                 args
             }
             Mac {
-                output_path,
+                in_out:
+                    InOutPaths {
+                        output_path,
+                        project_path,
+                    },
                 dry_run,
-                release,
-                ci,
-                project_path,
+                build_mode: BuildMode { release, ci },
             } => {
                 let mut args = vec!["-t", "mac", "-o", &*output_path.to_str().unwrap()];
 
@@ -381,21 +368,28 @@ impl BuildCommands {
                 args
             }
             M17n {
-                output_path,
-                release,
-                ci,
-                project_path,
+                in_out:
+                    InOutPaths {
+                        output_path,
+                        project_path,
+                    },
+                build_mode: BuildMode { release, ci },
             } => return Err(From::from("M17n isn't supported in Python".to_string())),
             X11 {
-                output_path,
-                release,
-                ci,
-                project_path,
+                in_out:
+                    InOutPaths {
+                        output_path,
+                        project_path,
+                    },
+                build_mode: BuildMode { release, ci },
             } => return Err(From::from("X11 isn't supported in Python".to_string())),
             Qr {
-                output_path,
+                in_out:
+                    InOutPaths {
+                        output_path,
+                        project_path,
+                    },
                 layout,
-                project_path,
             } => vec![
                 "-t",
                 "qr",
@@ -406,9 +400,12 @@ impl BuildCommands {
                 &*project_path.to_str().unwrap(),
             ],
             ErrorModel {
-                output_path,
+                in_out:
+                    InOutPaths {
+                        output_path,
+                        project_path,
+                    },
                 layout,
-                project_path,
             } => vec![
                 "-t",
                 "errormodel",
@@ -418,6 +415,7 @@ impl BuildCommands {
                 layout,
                 &*project_path.to_str().unwrap(),
             ],
+            X11RS { .. } => unreachable!("covered in previous match"),
         };
 
         if let Some(gh_username) = github_username {
@@ -446,16 +444,13 @@ fn launch_py_kbdgen(args: &[&str]) -> i32 {
         name: std::ffi::CString::new("language_tags").unwrap(),
         init_func: py_language_tags::PyInit_language_tags,
     };
-    
+
     let mod_logger = ExtensionModule {
         name: std::ffi::CString::new("rust_logger").unwrap(),
         init_func: py_logger::PyInit_rust_logger,
     };
 
-    config.extra_extension_modules = vec![
-        mod_language_tags,
-        mod_logger,
-    ];
+    config.extra_extension_modules = vec![mod_language_tags, mod_logger];
 
     // Construct a new Python interpreter using that config, handling any errors
     // from construction.
@@ -503,21 +498,25 @@ fn main() {
                 }
             }
         },
-        
+
         Commands::New { command } => match command {
-            NewCommands::Bundle { bundle_name, output_path } => {
-                match kbdgen::cli::from_cldr::cldr_to_kbdgen(&*output_path, &*bundle_name) {
-                    Ok(_) => std::process::exit(0),
-                    Err(e) => {
-                        eprintln!("{:?}", e);
-                        std::process::exit(1);
-                    }
+            NewCommands::Bundle {
+                bundle_name,
+                output_path,
+            } => match kbdgen::cli::from_cldr::cldr_to_kbdgen(&*output_path, &*bundle_name) {
+                Ok(_) => std::process::exit(0),
+                Err(e) => {
+                    eprintln!("{:?}", e);
+                    std::process::exit(1);
                 }
-            }
-            NewCommands::Layout { project_path, output_path } => {
+            },
+            NewCommands::Layout {
+                project_path,
+                output_path,
+            } => {
                 eprintln!("Not yet supported.");
                 std::process::exit(1)
             }
-        }
+        },
     }
 }
