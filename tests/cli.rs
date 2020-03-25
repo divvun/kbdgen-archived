0f15e3849dd8ddf0731e0890fb61ba0284d7db92
Pascal Hertleif <pascal@technocreatives.com>
2020-03-25 15:58:25 +0100

    Add simple test asserting XKB output works

---
+++ tests/cli.rs
@@ -0,0 +1,27 @@
+use assert_cmd::Command;
+use tempfile::tempdir;
+
+#[test]
+fn build_x11() {
+    logger();
+    let tmp = tempdir().unwrap();
+
+    Command::cargo_bin("kbdgen").unwrap()
+        .arg("build")
+        .arg("x11")
+        .arg("examples/sme.kbdgen")
+        .arg("--output")
+        .arg(tmp.path())
+        .assert()
+        .success();
+
+    assert!(tmp.path().join("linux").join("se-SE.xkb").exists());
+}
+
+pub fn logger() {
+    let _ = env_logger::Builder::from_default_env()
+        .filter(None, log::LevelFilter::Debug)
+        .target(env_logger::Target::Stderr)
+        .is_test(true)
+        .try_init();
+}
