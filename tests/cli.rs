143d2b79f51f4d232096910ac72329697f380739
Pascal Hertleif <pascal@technocreatives.com>
2020-03-25 16:00:22 +0100

    Add M17N smoketest

--- tests/cli.rs
+++ tests/cli.rs
@@ -18,6 +18,23 @@ fn build_x11() {
     assert!(tmp.path().join("linux").join("se-SE.xkb").exists());
 }
 
+#[test]
+fn build_m17n() {
+    logger();
+    let tmp = tempdir().unwrap();
+
+    Command::cargo_bin("kbdgen").unwrap()
+        .arg("build")
+        .arg("m17n")
+        .arg("examples/sme.kbdgen")
+        .arg("--output")
+        .arg(tmp.path())
+        .assert()
+        .success();
+
+    assert!(tmp.path().join("se-SE").join("win.mim").exists());
+}
+
 pub fn logger() {
     let _ = env_logger::Builder::from_default_env()
         .filter(None, log::LevelFilter::Debug)
